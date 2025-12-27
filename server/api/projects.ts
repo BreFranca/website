c93f13ef4b334ad6beb8ea4aea8550e5741d58bf
BreFranca <bre.sfranca@gmail.com>
2025-12-27 16:04:56 +0100

    fix: update event handler syntax for consistency

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,6 +1,6 @@
 import projectsData from '../data/projects.json'
 
-export default defineEventHandler(event => {
+export default defineEventHandler(() => {
   return {
     projects: projectsData.projects,
   }
