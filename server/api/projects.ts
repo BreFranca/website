b9c09dae3ea06b9dc7df3fc63f08c5027cd665e1
BreFranca <bre.sfranca@gmail.com>
2025-12-27 16:01:02 +0100

    fix: remove unused locale query handling in projects API

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,9 +1,6 @@
 import projectsData from '../data/projects.json'
 
 export default defineEventHandler(event => {
-  const query = getQuery(event)
-  const locale = (query.locale as string) || 'pt'
-
   return {
     projects: projectsData.projects,
   }
